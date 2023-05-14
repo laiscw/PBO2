5519d4bfb313164de0f7dabddbf419ca250a56cd
Lais Cakrawati_210511002 <126759046+laiscw@users.noreply.github.com>
2023-05-15 00:35:59 +0700

    Add files via upload

---
+++ Latihan6/Import ray.py
@@ -0,0 +1,13 @@
+print('Lais Cakrawati\n210511002\nT121A(R1)\n')
+import ray
+@ray.remote
+def worker(num):
+    """Fungsi untuk menjalankan tugas pada proses multiprocessing"""
+    print('Worker', num)
+if __name__ == '__main__':
+# Inisialisasi Ray
+    ray.init()
+# Buat task untuk setiap item secara parallel
+    results = [worker.remote(i) for i in range(5)]
+# Ambil hasil task secara parallel
+    ray.get(results)
---
+++ Latihan6/Threading.py
@@ -0,0 +1,17 @@
+print('Lais Cakrawati\n210511002\nT121A(R1)\n')
+import threading
+
+def worker(num):
+    """Fungsi untuk menjalankan tugas pada thread baru"""
+    print('Worker', num)
+if __name__ == '__main__':
+# Membuat list untuk menyimpan setiap thread
+    threads = []
+# Memulai setiap thread dengan menggunakan threading.Thread
+    for i in range(5):
+        t = threading.Thread(target=worker, args=(i,))
+        t.start()
+        threads.append(t)
+# Menunggu thread selesai
+    for t in threads:
+        t.join()
